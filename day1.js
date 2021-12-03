c0d5618484aeee85f20bc0d7fd072701490700db
Kenneth van der Werf <kennethv@spotify.com>
2021-12-03 17:41:08 +0100

    Add day 1 - part 2

--- day1.js
+++ day1.js
@@ -16,3 +16,26 @@
     .map((curr, idx, arr) => [arr[idx-1], curr])
     .filter(([prev, curr]) => prev && curr > prev);
 }
+
+{
+  const given = [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263,
+  ];
+
+  const res = given
+    .map((curr, idx, arr) => [arr[idx-2], arr[idx-1], curr])
+    .map(([first, second, third]) => first + second + third)
+    .map((curr, idx, arr) => [arr[idx-1], curr])
+    .filter(([prev, curr]) => prev && curr > prev);
+
+  console.log(res, res.length);
+}
